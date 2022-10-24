30b4e1ca4eade2b60850cc010a04ab24ecdef2a1
w <pink_oops@163.com>
2022-10-25 01:12:48 +0800

    feature:增加序列化枚举类

---
+++ wrpc-simple/src/main/java/com/w/wrpc/SerializationEnum.java
@@ -0,0 +1,34 @@
+package com.w.wrpc;
+
+/**
+ * @author wsy
+ * @date 2022/10/25 12:45 AM
+ * @Description
+ */
+public enum SerializationEnum {
+    /**
+     * 心跳事件写入的序列化方式 不需要序列化(因为body会为空)
+     */
+    HEARTBEAT(new Byte("1"), "心跳事件"),
+
+    /**
+     * JSON序列化方式
+     */
+    JSON(new Byte("2"), "JSON"),
+    ;
+    private Byte code;
+    private String desc;
+
+    SerializationEnum(Byte code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public Byte getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
